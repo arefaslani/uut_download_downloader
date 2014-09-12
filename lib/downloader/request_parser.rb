d2599d319a586a44e7f4fea4efb1af9c6770d43d
Aref Aslani <arefaslani@gmail.com>
2014-09-12 15:09:06 +0430

    remove extra unnecessary methods from request parser

--- lib/downloader/request_parser.rb
+++ lib/downloader/request_parser.rb
@@ -24,10 +24,6 @@ module Downloader
       self.headers['download_id']
     end
 
-    # def protocol_version
-    #   headers[2].split("\/")[1]
-    # end
-
     def body
       self.data.split("\n\n")[1].strip
     end
@@ -41,16 +37,5 @@ module Downloader
         self.headers[key.strip.downcase] = value.strip.downcase
       end
     end
-
-    # def headers
-    #   raw_headers = @data.split("\n\n")[0]
-    #   raw_headers.split("|")
-    # end
-
-    # def validate_request
-    #   raise Errors::InvalidRequestError unless headers[0].match(/^(http|https|ftp|ftps|bittorrent|magnet)$/i)
-    #   raise Errors::InvalidRequestError unless headers[1].match(/^\d+$/)
-    #   raise Errors::InvalidRequestError unless headers[2].match(/^UUTDOWNLOAD\/\d\.\d$/)
-    # end
   end
 end
