8514d455931bd1c58b1a349524c201a902b6bf8b
Aref Aslani <arefaslani@gmail.com>
2014-09-14 11:36:35 +0430

    render xml progress

--- lib/web/app.rb
+++ lib/web/app.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require 'sinatra/json'
+require 'nokogiri'
 
 class WebApp < Sinatra::Base
   get '/progress/:id/:format' do
@@ -7,6 +8,13 @@ class WebApp < Sinatra::Base
     when 'json'
       content_type :json
       json id: params[:id]
+    when 'xml'
+      content_type :xml
+      Nokogiri::XML::Builder.new do |xml|
+        xml.progress do
+          xml.id params[:id].to_s
+        end
+      end.to_xml.to_s
     end
   end
 end
