55af242dde111d20ccb3f325c31b8df78f09b922
Aref Aslani <arefaslani@gmail.com>
2014-09-15 13:20:30 +0430

    move binary commands to application class

--- lib/downloader.rb
+++ lib/downloader.rb
@@ -1,5 +1,18 @@
 require "downloader/version"
+require 'eventmachine'
+require 'downloader/request_handler'
 
 module Downloader
-  # Your code goes here...
+  class Application
+    def self.run
+      puts "Server Started..."
+      begin
+        EM.run do
+          EM.start_server('0.0.0.0', ENV['PORT'].to_i, Downloader::RequestHandler)
+        end
+      rescue Interrupt
+        puts "Server Stopped."
+      end
+    end
+  end
 end
