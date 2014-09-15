6137cea02f968ac69a61e537238793929c7ba752
Aref Aslani <arefaslani@gmail.com>
2014-09-15 13:45:23 +0430

    add option parser method

--- lib/downloader.rb
+++ lib/downloader.rb
@@ -1,10 +1,13 @@
 require "downloader/version"
 require 'eventmachine'
 require 'downloader/request_handler'
+require 'optparse'
 
 module Downloader
   class Application
+
     def self.run
+      get_options
       puts "Server Started..."
       begin
         EM.run do
@@ -13,6 +16,33 @@ module Downloader
       rescue Interrupt
         puts "Server Stopped."
       end
+    end
+
+    def self.get_options
+
+      OptionParser.new do |opts|
+        opts.banner = "Usage: downloader [options]"
+
+        opts.on("-d", "--downloads-dir DIR", "Downloaded files go here.") do |dir|
+          ENV['DOWNLOAD_PATH'] = dir
+        end
+
+        opts.on("-P", "--port PORT", "Set main process listener port.") do |port|
+          ENV['PORT'] = port
+        end
+
+        opts.on("-p", "--port-web PORT", "Set web interface listener port.") do |port|
+          ENV['PORT_WEB'] = port
+        end
+
+        opts.on("-t", "--tracker-address ADDRESS", "Set the torrent tracker address.") do |addresss|
+          ENV['TRACKER_ADDRESS'] = addresss
+        end
+
+        opts.on("-T", "--tracker-port PORT", "Set the torrent tracker port.") do |port|
+          ENV['TRACKER_PORT'] = port
+        end
+      end.parse!
     end
   end
 end
