eadc9cad1d53161bce281461084b32a2565daff3
Aref Aslani <arefaslani@gmail.com>
2014-09-15 08:04:19 +0430

    get download progress from the web interface

--- lib/web/app.rb
+++ lib/web/app.rb
@@ -3,16 +3,19 @@ require 'sinatra/json'
 require 'nokogiri'
 
 class WebApp < Sinatra::Base
-  get '/progress/:id.?:format?' do
-    case params[:format]
+  get '/progress/:id.?:format?' do |id, format|
+    progress = File.read(File.expand_path(File.join('logs/', "#{id}.txt")))
+
+    case format
     when 'json'
       content_type :json
-      json id: params[:id]
+      json id: id, progress: progress
     when 'xml'
       content_type :xml
       Nokogiri::XML::Builder.new do |xml|
-        xml.progress do
-          xml.id params[:id].to_s
+        xml.file do
+          xml.id id
+          xml.progress progress
         end
       end.to_xml.to_s
     end
