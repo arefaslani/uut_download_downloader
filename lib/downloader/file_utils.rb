5e76c650d4f7f37753d91c35ea6cf8299ad0eec7
Aref Aslani <arefaslani@gmail.com>
2014-09-21 02:48:54 +0430

    delete file loggers from Downloader::FileUtils

--- lib/downloader/file_utils.rb
+++ lib/downloader/file_utils.rb
@@ -8,17 +8,6 @@ module Downloader
       return File.join(download_path, downloaded_file_name)
     end
 
-    def self.write_log(id, message, options={})
-      options[:mode] ||= 'a'
-      File.open(File.expand_path(File.join(File.dirname(__FILE__), '../../logs', "#{id}.txt")), options[:mode]) do |file|
-        file.write(message)
-      end
-    end
-
-    def self.delete_progress_file(id)
-      File.delete(File.join(File.dirname(__FILE__), "../../logs/#{id}.txt"))
-    end
-
     def self.generate_torrent(id, url)
       system("mktorrent -o #{self.generate_download_path(id, url)}.torrent \\
               -a udp://#{ENV['TRACKER_ADDRESS']}:#{ENV['TRACKER_PORT']}/announce \\
